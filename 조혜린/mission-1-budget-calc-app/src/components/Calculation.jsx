00a59019ed3c3f3a07edcbea206f1ce5692723dd
hyer0705 <hyer1205@gmail.com>
2024-05-10 02:44:15 +0900

    add: 예산 계산기 UI CSS 스타일링

--- 조혜린/mission-1-budget-calc-app/src/components/Calculation.jsx
+++ 조혜린/mission-1-budget-calc-app/src/components/Calculation.jsx
@@ -1,50 +1,52 @@
 import { IoMdSend } from "react-icons/io";
 import { MdEdit, MdDeleteForever } from "react-icons/md";
 
+import styles from "./Calculation.module.css";
+
 export default function Calculation() {
   return (
     <>
       <header>
         <h1>예산 계산기</h1>
       </header>
-      <main>
-        <form>
-          <div>
+      <main className={styles.main}>
+        <form className={styles.form}>
+          <div className={styles.inputBudget}>
             <label htmlFor="expense">지출 항목</label>
-            <input id="expense" type="text" placeholder="예) 식비" />
             <label htmlFor="cost">비용</label>
+            <input id="expense" type="text" placeholder="예) 식비" />
             <input id="cost" type="number" value={0} />
           </div>
-          <button>
+          <button className={styles.btn}>
             제출 <IoMdSend />
           </button>
         </form>
         <div>
-          <ul>
+          <ul className={styles.budgets}>
             <li>
-              <span>식비</span>
-              <span>1000</span>
-              <div>
+              <span className={styles.budgetTitle}>식비</span>
+              <span className={styles.budgetCost}>1000</span>
+              <div className={styles.budgetBtns}>
                 <MdEdit />
                 <MdDeleteForever />
               </div>
             </li>
             <li>
-              <span>교통비</span>
-              <span>1500</span>
-              <div>
+              <span className={styles.budgetTitle}>교통비</span>
+              <span className={styles.budgetCost}>1000</span>
+              <div className={styles.budgetBtns}>
                 <MdEdit />
                 <MdDeleteForever />
               </div>
             </li>
           </ul>
-          <button>
+          <button className={`${styles.btn} ${styles.listDelBtn}`}>
             목록 지우기 <MdDeleteForever />
           </button>
         </div>
       </main>
       <footer>
-        <h2>총 지출: 900원</h2>
+        <h2 style={{ textAlign: "end" }}>총 지출: 900원</h2>
       </footer>
     </>
   );
