d5298baf94481eb1fa668f6dd11e5e8766940090
raon9401 <raon9401@naver.com>
2024-05-10 00:54:22 +0900

    Add: 예산 계산기 기능 및 디자인

---
+++ jhc/src/App.js
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import DeleteBtn from "./common/Button/DeleteBtn/DeleteBtn";
+import SubmitBtn from "./common/Button/SubmitBtn/SubmitBtn";
+import ContentsInput from "./components/ContentsInput/ContentsInput";
+import ListContents from "./components/ListContents/ListContents";
+import ContentsStateBar from "./components/ContentsStateBar/ContentsStateBar";
+
+function App() {
+  const [inputName, setInputName] = useState("");
+  const [inputCost, setInputCost] = useState(0);
+  const [contentsList, setContentsList] = useState([]);
+
+  // 수정 관련 state
+  const [isModify, setIsModify] = useState(false);
+  const [selectContentsIdx, setSelectContentsIdx] = useState();
+
+  // contnets modify, submit, delete 상태 state
+  const [contentsState, setContentsState] = useState();
+
+  const handleContentsList = () => {
+    setInputName("");
+    setInputCost(0);
+
+    if (isModify) {
+      const copyContentsList = [...contentsList];
+      copyContentsList[selectContentsIdx] = {
+        name: inputName,
+        cost: inputCost,
+      };
+      setContentsList([...copyContentsList]);
+      setIsModify(false);
+      setContentsState("modify");
+    } else {
+      setContentsList([...contentsList, { name: inputName, cost: inputCost }]);
+      setContentsState("submit");
+    }
+  };
+
+  const handleResetContentsList = () => {
+    setContentsList([]);
+  };
+
+  const handleTotalCost = () => {
+    return contentsList.reduce((acc, cur) => acc + cur.cost, 0);
+  };
+
+  useEffect(() => {
+    if (isModify) {
+      setInputName(contentsList[selectContentsIdx]?.name);
+      setInputCost(contentsList[selectContentsIdx]?.cost);
+    }
+    // eslint-disable-next-line
+  }, [isModify]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setContentsState();
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [contentsState]);
+
+  return (
+    <div className="App">
+      {contentsState && <ContentsStateBar contentsState={contentsState} />}
+      <h1 className="title-text">예산 계산기</h1>
+      <section className="main">
+        <article className="input-area">
+          <ContentsInput
+            inputName={inputName}
+            inputCost={inputCost}
+            setInputName={setInputName}
+            setInputCost={setInputCost}
+          />
+        </article>
+        <article>
+          <SubmitBtn
+            handleContentsList={handleContentsList}
+            isModify={isModify}
+          />
+        </article>
+        <article className="contents-area">
+          <ListContents
+            contentsList={contentsList}
+            setContentsList={setContentsList}
+            setSelectContentsIdx={setSelectContentsIdx}
+            setIsModify={setIsModify}
+            setContentsState={setContentsState}
+          />
+        </article>
+        <article>
+          {contentsList.length !== 0 && (
+            <DeleteBtn handleResetContentsList={handleResetContentsList} />
+          )}
+        </article>
+      </section>
+      <h2 className="cost-text">
+        총지출: {contentsList.length !== 0 ? handleTotalCost() : 0}원
+      </h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ jhc/src/components/ContentsInput/ContentsInput.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const ContentsInput = ({
+  inputName,
+  inputCost,
+  setInputName,
+  setInputCost,
+}) => {
+  return (
+    <>
+      <div className="input-box">
+        <span>지출 항목</span>
+        <input
+          className="contents-input"
+          type="text"
+          placeholder="예) 렌트비"
+          onChange={(e) => setInputName(e.target.value)}
+          value={inputName}
+        />
+      </div>
+      <div className="input-box">
+        <span>비용</span>
+        <input
+          className="contents-input"
+          type="number"
+          value={inputCost}
+          onChange={(e) => setInputCost(Number(e.target.value))}
+        />
+      </div>
+    </>
+  );
+};
+
+export default ContentsInput;
